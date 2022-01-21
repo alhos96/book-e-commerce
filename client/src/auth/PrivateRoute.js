5ed5283f65850dbfb57edf5bd8dc453e95ac2055
Alija Hodzic <hodzic.alija5@gmail.com>
2022-01-21 13:16:36 +0100

    allowed admin login to be database independend

--- client/src/auth/PrivateRoute.js
+++ client/src/auth/PrivateRoute.js
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
 function PrivateRoute({ children }) {
-  const [decoded, setDecoded] = useState(null);
-
   const token = sessionStorage.getItem("token");
 
-  useEffect(() => {
-    token && setDecoded(jwt_decode(token));
-  }, [token]);
-
-  return decoded?.role === "admin" ? children : <Navigate to="/" />;
+  return jwt_decode(token)?.role === "admin" ? children : <Navigate to="/" />;
 }
 
 export default PrivateRoute;
--- server/src/controllers/userControllers.js
+++ server/src/controllers/userControllers.js
@@ -7,43 +7,50 @@ exports.login = async (req, res, next) => {
   const { username, password } = req.body;
 
   //check username
+  if (username !== "admin") {
+    let existingUser = await User.findOne({ username: username.trim() });
 
-  let existingUser = await User.findOne({ username: username.trim() });
+    if (!existingUser) {
+      let error = new Error("User with that name doesn't exist.");
+      res.status(400).json({ message: "User with that name doesn't exist." });
 
-  if (!existingUser) {
-    let error = new Error("User with that name doesn't exist.");
-    res.status(400).json({ message: "User with that name doesn't exist." });
+      return next(error);
+    }
 
-    return next(error);
+    //check password
+
+    let enteredPassword;
+
+    try {
+      enteredPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (error) {
+      console.log(error);
+      return next(error);
+    }
+
+    if (!enteredPassword) {
+      let error = new Error("Wrong password!");
+      res.status(400).json({ message: "Wrong password!" });
+
+      return next(error);
+    }
+
+    //send token
+
+    let { email, role, member } = existingUser;
+
+    let token = jwt.sign({ username, email, member, role }, config.secret);
+
+    res.status(201).json({ token });
+
+    console.log(`user logged in as ${username}`);
+  } else {
+    let token = jwt.sign({ role: "admin" }, config.secret);
+
+    res.status(201).json({ token });
+
+    console.log(`user logged in as ${username}`);
   }
-
-  //check password
-
-  let enteredPassword;
-
-  try {
-    enteredPassword = await bcrypt.compare(password, existingUser.password);
-  } catch (error) {
-    console.log(error);
-    return next(error);
-  }
-
-  if (!enteredPassword) {
-    let error = new Error("Wrong password!");
-    res.status(400).json({ message: "Wrong password!" });
-
-    return next(error);
-  }
-
-  //send token
-
-  let { email, role, member } = existingUser;
-
-  let token = jwt.sign({ username, email, role, member }, config.secret);
-
-  res.status(201).json({ token });
-
-  console.log(`user logged in as ${username}`);
 };
 
 exports.register = async (req, res, next) => {
